108345b904c4b4b731ceb8cea2d3d0cd6e3ccc94
Aman Chauhan <61115039+amanchauhan71@users.noreply.github.com>
2021-03-30 10:20:36 +0530

    Add files via upload

---
+++ Turtle-race-start/turtle-race-start.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle  win the race")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+
+all_turtle = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtle.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You have won! The {winning_color} turtle is the winner")
+
+            else:
+                print(f"You have lost! The {winning_color} turtle is the winner")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+screen.exitonclick()
